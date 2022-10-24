fa2c83449ada27ed6ad1bb34be7dc16d9884e4db
Thomaz Suzuki <thomaz.szk@gmail.com>
2022-10-24 01:00:11 -0300

    Login Functionality

--- Robinho_Webapp/app.py
+++ Robinho_Webapp/app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 app.secret_key = 'supersecrete_key123'
 
 
+@app.route('/')
+def home():
+    return render_template("login.html") 
+
 @app.route("/hello")
 def index():
     flash("What's your name?")
@@ -13,3 +17,10 @@ def index():
 def greet(): 
     flash("Hi " + str(request.form['name_input']) + ", great to see you!")
     return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
