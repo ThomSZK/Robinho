9db47c143ed3e338b0439031a5085d24d888c88b
Thomaz Suzuki <thomaz.szk@gmail.com>
2022-10-25 00:50:12 -0300

    Login and Registration db connection

--- Robinho_Webapp/app.py
+++ Robinho_Webapp/app.py
@@ -1,43 +1,82 @@
+from operator import length_hint
 from this import d
-from flask import Flask, render_template, request, flash
+import bcrypt
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-# from flask_wtf import wtforms
-# from wtforms import StringField, PasswordField, SubmitField
-# from wtforms.validators import InputRequired, Length, ValidationError
+from flask_wtf import FlaskForm
+from pyparsing import StringEnd
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.secret_key = 'supersecrete_key123'
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:Thom_SZK_127789*@localhost:5432/robinho?options=-c%20search_path=schema_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:Thom_SZK_127789*@localhost:5432/robinho' #?options=-c%20search_path=schema_name
 db = SQLAlchemy(app)
 
+
+bcrypt = Bcrypt(app)
 
 class Rob_User(db.Model):
     __table_args__ = {'schema' : 'rob'}
     __tablename__ = "Rob_User"
-    User_ID = db.Column(db.INTEGER, primary_key = True)
-    User_Acc = db.Column(db.VARCHAR(255), unique = True)
+    User_ID = db.Column(db.INTEGER, primary_key = True, nullable = False)
+    User_Acc = db.Column(db.VARCHAR(255), unique = True, nullable = False)
     User_Password = db.Column(db.VARCHAR(255))
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=2, max=255)], render_kw={"placeholder" : "Username"})
+    
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=255)], render_kw={"placeholder" : "Password"})
+    
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = Rob_User.query.filter_by(username = username.data).first()
+
+        if existing_user_username:
+            raise ValidationError("Usuario ja existente. Por favor escolha outro.")
+            
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=2, max=255)], render_kw={"placeholder" : "Username"})
+    
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=255)], render_kw={"placeholder" : "Password"})
+    
+    submit = SubmitField("Login")
+
 
 
 @app.route('/')
 def select():
     solution = Rob_User.query.all()
     print(solution)
-    return 'Nao sei'
+    return 'no idea'
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    return render_template("login.html", form=form)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html") 
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = Rob_User(username = form.username.data, pasword = hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template("register.html", form=form) 
 
 
 @app.route("/hello")
