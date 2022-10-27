cf2c4890b6731274d1e54d5154769b4eb32a999d
Yasmim Skroch <yasmim_nathaly@hotmail.com>
2022-10-27 09:22:36 -0300

    Arquivo com definição dos blocos

---
+++ Robinho_Webapp/templates/js/blocos.js
@@ -0,0 +1,459 @@
+Blockly.Blocks['mover'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("MOVER");
+        this.appendDummyInput()
+            .appendField("Direcao")
+            .appendField(new Blockly.FieldDropdown([["Norte", "N"], ["Sul", "S"], ["Leste", "L"], ["Oeste", "O"]]), "direcao");
+        this.appendDummyInput()
+            .appendField("Quantidade")
+            .appendField(new Blockly.FieldNumber(1, 1, 15), "quantidade");
+        this.setInputsInline(false);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(180);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['for_repetir'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("repetir")
+            .appendField(new Blockly.FieldNumber(2, 1), "i")
+            .appendField("vezes");
+        this.appendStatementInput("NAME")
+            .setCheck(null);
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(270);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['while_enquanto'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("enquanto");
+        this.appendValueInput("a")
+            .setCheck(null);
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldDropdown([["=", "="], ["!=", "!="], ["<", "<"], ["<=", "<="], [">", ">"], [">=", ">="]]), "tipo");
+        this.appendValueInput("b")
+            .setCheck(null);
+        this.appendStatementInput("NAME")
+            .setCheck(null)
+            .appendField("faca");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(330);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['ler_distancia'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("ler distancia");
+        this.setOutput(true, "Number");
+        this.setColour(60);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['ler_cor'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("ler cor");
+        this.setOutput(true, null);
+        this.setColour(330);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['acender_led'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("acender led")
+            .appendField(new Blockly.FieldColour("#ffff33"), "cor");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(345);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['virar'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("virar")
+            .appendField(new Blockly.FieldAngle(90), "angulo");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(105);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['se_senao'] = {
+    init: function () {
+        this.appendValueInput("se")
+            .setCheck("Boolean")
+            .appendField("se");
+        this.appendStatementInput("faca")
+            .setCheck(null)
+            .appendField("faca");
+        this.appendDummyInput()
+            .appendField("se nao");
+        this.appendStatementInput("faca_senao")
+            .setCheck(null)
+            .appendField("faca");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(195);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['camera_cor'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("cor da camera");
+        this.setOutput(true, null);
+        this.setColour(230);
+        this.setTooltip("Retorna a cor lida pela câmera");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['abrir_garra'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("abrir garra");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(0);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['fechar_garra'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("fechar garra");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(0);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['atribuicao_variavel'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldVariable("var"), "var")
+            .appendField("=");
+        this.appendValueInput("valor")
+            .setCheck(null);
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(180);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['variavel'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldVariable("var"), "var");
+        this.setOutput(true, null);
+        this.setColour(180);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Blocks['cor'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField(new Blockly.FieldColour("#ffff33"), "NAME");
+        this.setOutput(true, null);
+        this.setColour(315);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+var toolbox = {
+    "kind": "categoryToolbox",
+    "contents": [
+        {
+            "kind": "category",
+            "name": "Logica",
+            "colour": "0",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "se_senao"
+                },
+                {
+                    "kind": "block",
+                    "type": "logic_compare"
+                },
+                {
+                    "kind": "block",
+                    "type": "logic_operation"
+                },
+                {
+                    "kind": "block",
+                    "type": "logic_boolean"
+                },
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Loop",
+            "colour": "45",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "for_repetir"
+                },
+                {
+                    "kind": "block",
+                    "type": "while_enquanto"
+                },
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Matematica",
+            "colour": "90",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "math_arithmetic"
+                },
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Campos",
+            "colour": "135",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "math_number",
+                    "fields": {
+                        "NUM": 123
+                    }
+                },
+                {
+                    "kind": "block",
+                    "type": "text"
+                },
+                {
+                    "kind": "block",
+                    "type": "cor"
+                },
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Funcoes",
+            "colour": "180",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "mover"
+                },
+                {
+                    "kind": "block",
+                    "type": "ler_distancia"
+                },
+                {
+                    "kind": "block",
+                    "type": "ler_cor"
+                },
+                {
+                    "kind": "block",
+                    "type": "acender_led"
+                },
+                {
+                    "kind": "block",
+                    "type": "camera_cor"
+                },
+                {
+                    "kind": "block",
+                    "type": "abrir_garra"
+                },
+                {
+                    "kind": "block",
+                    "type": "fechar_garra"
+                },
+            ]
+        },
+        {
+            "kind": "category",
+            "name": "Variaveis",
+            "colour": "225",
+            "contents": [
+                {
+                    "kind": "block",
+                    "type": "atribuicao_variavel"
+                },
+                {
+                    "kind": "block",
+                    "type": "variavel"
+                },
+            ]
+        },
+    ]
+};
+
+var demoWorkspace = Blockly.inject('blocklyDiv',
+    {
+        media: 'https://unpkg.com/blockly/media/',
+        toolbox: toolbox
+    });
+
+Blockly.Python['mover'] = function (block) {
+    var dropdown_direcao = block.getFieldValue('direcao');
+    var number_quantidade = block.getFieldValue('quantidade');
+    // TODO: Assemble Python into code variable.
+    var code = '...\n';
+    return code;
+};
+
+Blockly.Python['for_repetir'] = function (block) {
+    var number_i = block.getFieldValue('i');
+    var statements_name = Blockly.Python.statementToCode(block, 'NAME');
+    // TODO: Assemble Python into code variable.
+    var code = '...\n';
+    return code;
+};
+
+Blockly.Python['while_enquanto'] = function (block) {
+    var value_a = Blockly.Python.valueToCode(block, 'a', Blockly.Python.ORDER_ATOMIC);
+    var dropdown_tipo = block.getFieldValue('tipo');
+    var value_b = Blockly.Python.valueToCode(block, 'b', Blockly.Python.ORDER_ATOMIC);
+    var statements_name = Blockly.Python.statementToCode(block, 'NAME');
+    // TODO: Assemble Python into code variable.
+    var code = '...\n';
+    return code;
+};
+
+Blockly.Python['ler_distancia'] = function (block) {
+    // TODO: Assemble Python into code variable.
+    var code = '...';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['ler_cor'] = function (block) {
+    // TODO: Assemble Python into code variable.
+    var code = '...';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['acender_led'] = function (block) {
+    var colour_cor = block.getFieldValue('cor');
+    // TODO: Assemble Python into code variable.
+    var code = '...\n';
+    return code;
+};
+
+Blockly.Python['virar'] = function (block) {
+    var angle_angulo = block.getFieldValue('angulo');
+    // TODO: Assemble Python into code variable.
+    var code = '...\n';
+    return code;
+};
+
+Blockly.Python['se_senao'] = function (block) {
+    var value_se = Blockly.Python.valueToCode(block, 'se', Blockly.Python.ORDER_ATOMIC);
+    var statements_faca = Blockly.Python.statementToCode(block, 'faca');
+    var statements_faca_senao = Blockly.Python.statementToCode(block, 'faca_senao');
+    // TODO: Assemble Python into code variable.
+    var code = '...\n';
+    return code;
+};
+
+Blockly.Python['camera_cor'] = function (block) {
+    // TODO: Assemble Python into code variable.
+    var code = '...';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['abrir_garra'] = function (block) {
+    // TODO: Assemble Python into code variable.
+    var code = '...\n';
+    return code;
+};
+
+Blockly.Blocks['fechar_garra'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("fechar garra");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(0);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Python['atribuicao_variavel'] = function (block) {
+    var variable_var = Blockly.Python.nameDB_.getName(block.getFieldValue('var'), Blockly.Variables.NAME_TYPE);
+    var value_valor = Blockly.Python.valueToCode(block, 'valor', Blockly.Python.ORDER_ATOMIC);
+    // TODO: Assemble Python into code variable.
+    var code = '...\n';
+    return code;
+};
+
+Blockly.Python['variavel'] = function (block) {
+    var variable_var = Blockly.Python.nameDB_.getName(block.getFieldValue('var'), Blockly.Variables.NAME_TYPE);
+    // TODO: Assemble Python into code variable.
+    var code = '...';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_NONE];
+};
+
+Blockly.Python['cor'] = function (block) {
+    var colour_name = block.getFieldValue('NAME');
+    // TODO: Assemble Python into code variable.
+    var code = '...';
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.Python.ORDER_NONE];
+};
+
+
+//Aumentar tamanho do workspace
+var onresize = function (e) {
+    var blocklyArea = document.getElementById('blocklyArea');
+    var blocklyDiv = document.getElementById('blocklyDiv');
+    var element = blocklyArea;
+    // Position blocklyDiv over blocklyArea.
+    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+    Blockly.svgResize(demoWorkspace);
+};
+window.addEventListener('resize', onresize, false);
+onresize();
+Blockly.svgResize(demoWorkspace);
