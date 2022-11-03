2d1c957e2df6101587dbe09582bfe5f834d3fd80
Thomaz Suzuki <thomaz.szk@gmail.com>
2022-11-03 03:20:08 -0300

    upython initial files

---
+++ MicroPython/main.py
@@ -0,0 +1,18 @@
+import machine
+from machine import Pin
+import time
+import camera
+
+flash = Pin(4, Pin.OUT)
+for i in range(3):
+    flash.value(1)
+    time.sleep(0.1)
+    flash.value(0)
+    time.sleep(0.1)
+    print("desligou")
+    
+#camera.init(10)
+#camera.quality(12)
+#camera.framesize(9)
+    
+#test test test
