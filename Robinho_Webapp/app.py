ae4e1e3fb324cb4a8ff94f07ce8a9d1b58e93aca
Martoto <baronwalrus@gmail.com>
2022-11-14 22:41:56 +0000

    Corrigido live streaming + nomes incorretos PostGres

--- Robinho_Webapp/app.py
+++ Robinho_Webapp/app.py
@@ -3,7 +3,7 @@ from operator import length_hint
 from this import d
 import bcrypt
 from django.shortcuts import render
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user, user_accessed
 from flask_wtf import FlaskForm
@@ -12,13 +12,17 @@ from pyparsing import StringEnd
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
-
+#from RobinhoImageProcessing import main as img
+#from RobinhoImageProcessing.utils import detector, superimpose as si
+import cv2
+import numpy as np
+import time
 
 app = Flask(__name__)
 app.secret_key = 'supersecrete_key123'
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:Thom_SZK_127789*@localhost:5432/robinho' #?options=-c%20search_path=schema_name
+password = "Thom_SZK_127789*"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:robinho@localhost:5432/robinho' #?options=-c%20search_path=schema_name
 db = SQLAlchemy(app)
 
 
@@ -29,20 +33,38 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 
 
+def genFrames():  
+    #camera = cv2.VideoCapture(-1)
+    frames=0
+    while True:
+        #success, frame = camera.read()  # read the camera frame
+        #if not success:
+            #break
+        #else:
+        frames+=1
+        frame = cv2.imread('./images/feed.png')
+        time.sleep(0.5)
+        if frame is not None:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
 @login_manager.user_loader
-def load_user(User_ID):
-    return Rob_User.query.get(int(User_ID))
+def load_user(user_id):
+    return Rob_User.query.get(int(user_id))
 
 
 class Rob_User(db.Model, UserMixin):
-    __table_args__ = {'schema' : 'rob'}
-    __tablename__ = "Rob_User"
-    User_ID = db.Column(db.INTEGER, primary_key = True, nullable = False)
-    User_Acc = db.Column(db.VARCHAR(255), unique = True, nullable = False)
-    User_Password = db.Column(db.VARCHAR(255))
+    __table_args__ = {'schema' : 'public'}
+    __tablename__ = "rob_user"
+    user_id = db.Column(db.INTEGER, primary_key = True, nullable = False)
+    user_acc = db.Column(db.VARCHAR(255), unique = True, nullable = False)
+    user_password = db.Column(db.VARCHAR(255))
 
     def get_id(self):
-        return self.User_ID
+        return self.user_id
 
 
 class RegisterForm(FlaskForm):
@@ -74,18 +96,15 @@ def set_password(pw):
 
 @app.route('/')
 def select():
-    solution = Rob_User.query.all()
-    print(solution)
-    return 'no idea'
-
+    return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = Rob_User.query.filter_by(User_Acc = form.User_Acc.data).first()
+        user = Rob_User.query.filter_by(user_acc = form.User_Acc.data).first()
         if user:
-            if bcrypt.check_password_hash(user.User_Password, form.User_Password.data):
+            if bcrypt.check_password_hash(user.user_password, form.User_Password.data):
                 login_user(user, remember=True)
                 return redirect(url_for('dashboard'))
     return render_template("login.html", form=form)
@@ -119,12 +138,15 @@ def tarefa_professor():
 def tarefa_professor_bloco():
     return render_template('tarefas-professor-blocos.html')
 
+@app.route('/video_feed')
+def video_feed():
+    return Response(genFrames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/lista_espera', methods=['GET', 'POST'])
 @login_required
 def lista_espera():
     return render_template('lista-de-espera.html')
-
+ 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -132,7 +154,7 @@ def register():
 
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.User_Password.data).decode('utf8')
-        new_user = Rob_User(User_Acc = form.User_Acc.data, User_Password = hashed_password)
+        new_user = Rob_User(user_acc = form.User_Acc.data, user_password = hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
@@ -163,7 +185,7 @@ def greet():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='192.168.100.253')
 
 
 
