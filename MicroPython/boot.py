c0c181bfaeeb287f48684198a35f0a6b5bfc3a6b
Thomaz Suzuki <thomaz.szk@gmail.com>
2022-11-15 19:08:42 -0300

    Micropython Changes

--- MicroPython/boot.py
+++ MicroPython/boot.py
@@ -11,8 +11,7 @@ import camera
 webrepl.start()
 sta_if = network.WLAN(network.STA_IF); sta_if.active(True)
 sta_if.connect("It hurts when IP 2.4","SzK$45%63@")
-uart = machine.UART(1, 9600 , rx=12, tx=13)
-uart.init(9600, bits=8, parity=None, stop=1)
+#uart = machine.UART(1, 9600 , rx=12, tx=13)
+#uart.init(9600, bits=8, parity=None, stop=1)
 
 
-#test test
