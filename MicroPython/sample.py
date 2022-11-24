763acaf6d354d84adf6e2c6d2d5b404f85a5cf34
Martoto <baronwalrus@gmail.com>
2022-11-24 02:00:19 +0000

    samply.py

--- MicroPython/sample.py
+++ MicroPython/sample.py
@@ -1,4 +1,6 @@
+robinho_func.arduino_cmd(0x98, uart)
 robinho_func.arduino_cmd(0x1F, uart)
+robinho_func.arduino_cmd(0xb8, uart)
 
 robinho_func.arduino_cmd(0x1C, uart)
 robinho_func.arduino_cmd(0x1C, uart)
